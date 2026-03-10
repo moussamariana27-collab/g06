824226dba80d7b39630ce051edfb4500c541061e
cauan <cauanr935@gmail.com>
2026-03-10 14:51:27 -0300

    #204 comentando código de personagens.js

--- src/scenes/estabelecimentos/SalaoDeBeleza.js
+++ src/scenes/estabelecimentos/SalaoDeBeleza.js
@@ -1,3 +1,4 @@
+// Cria a class SalaoDeBeleza
 class SalaoDeBeleza extends Phaser.Scene {
     constructor() {
         super({ key: 'SalaoDeBeleza' });
@@ -8,16 +9,19 @@ class SalaoDeBeleza extends Phaser.Scene {
     }
 
     create() {
+        // Cria a imagem de fundo do salão de beleza
         const bg = this.add.image(0, 0, 'bgSalaoDeBeleza').setOrigin(0, 0);
+        // Ajusta o tamanho da imagem de fundo 
         bg.setDisplaySize(this.scale.width, this.scale.height);
-
+        // Adiciona o texto entre aspas simples na tela e define a posição desse texto
         this.add.text(this.scale.width / 2, this.scale.height - 40, 'Pressione ESPAÇO para voltar', {
             fontSize: '16px', fill: '#ffffff',
             backgroundColor: '#000000',
             padding: { x: 10, y: 6 }
         }).setOrigin(0.5);
-
+        // Adiciona uma função que sempre que o jogador apertar a tecla SPACE ele retorna para a cidade
         this.input.keyboard.once('keydown-SPACE', () => this.scene.start('Cidade'));
+        // Adiciona uma função que sempre que o jogador apertar a tecla ENTER ele retorna para a cidade
         this.input.keyboard.once('keydown-ENTER', () => this.scene.start('Cidade'));
     }
 }
--- src/scenes/menu/personagens.js
+++ src/scenes/menu/personagens.js
@@ -14,16 +14,18 @@ class CharacterSelectScene extends Phaser.Scene {
     }
 
     create() {
+        // Obtém as coordenadas do centro da câmera principal
         const { centerX, centerY } = this.cameras.main;
 
         this.add.image(this.scale.width / 2, this.scale.height / 2, "fundo2")
+            // Faz a imagem se adequar ao tamanho da tela em largura e altura
             .setDisplaySize(this.scale.width, this.scale.height);
-
+        // Adiciona o texto entre aspas e define estilo
         this.add.text(centerX, 80, "Conheça seu Personagem", {
             fontSize: "60px",
             fill: "#000000"
         }).setOrigin(0.5);
-
+        // Adiciona texto entre aspas e define estilo da letra
         this.add.text(centerX, 150, "Clique em cima do personagem desejado para saber mais informações sobre ele", {
             fontSize: "32px",
             fill: "#000000",
@@ -31,15 +33,17 @@ class CharacterSelectScene extends Phaser.Scene {
             wordWrap: { width: 900 }
         }).setOrigin(0.5);
 
+        // Cria uma variável que guarda uma lista com o nome dos personagens jogáveis 
         let personagens = ['JOSÉ', 'PAULA', 'MARIA', 'JOÃO'];
+        // Define a distância horizontal entre os personagens 
         let espacamento = 200;
+        // Centraliza os personagens na tela
         let startX = centerX - (espacamento * (personagens.length - 1)) / 2;
-
         personagens.forEach((personagem, i) => {
             this.createCharacter(startX + (i * espacamento), centerY + 50, personagem);
         });
     }
-
+    // Define o tamanho do personagem se o personagem for JOSÉ ou JOÃO o tamanho da imagem diminui
     createCharacter(x, y, key) {
         let escala = 0.5;
 
@@ -51,7 +55,7 @@ class CharacterSelectScene extends Phaser.Scene {
             fontSize: "40px",
             fill: "#000000"
         }).setOrigin(0.5);
-
+        // Permite a escolha do personagem jogável
         let character = this.add.image(x, y, key).setScale(escala);
         character.setInteractive({ cursor: 'pointer' });
 
