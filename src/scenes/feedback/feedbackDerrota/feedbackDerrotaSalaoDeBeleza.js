041ae7f1e28090aedb34429fa28ce022b3aeed9c
unknown <fabianne.leite@sou.inteli.edu.br>
2026-03-25 15:08:26 -0300

    #359 Implementa o diálogo de feddback de vitória no salão de beleza

--- src/scenes/feedback/feedbackDerrota/feedbackDerrotaSalaoDeBeleza.js
+++ src/scenes/feedback/feedbackDerrota/feedbackDerrotaSalaoDeBeleza.js
@@ -7,5 +7,14 @@ class FeedbackDerrotaSalaoDeBeleza extends FeedbackDerrota {
     }
 
     definirFundo()    { return 'assets/salaodebeleza_interior.png'; }
-    definirDialogos() { return ['Texto de feedback de derrota do salao de beleza...']; }
+    definirDialogos() 
+    { return [
+                'Você deixou passar a oportunidade de conectar as dores da cliente com as soluções da Cielo.',
+                'A Dona Leila destacou preocupações com taxas no crédito, controle do parcelamento e impacto no faturamento, e o ideal era mostrar que existem soluções para equilibrar custos e aumentar as vendas com segurança.',
+                'Quando ela trouxe dúvidas sobre o recebimento dos valores, era esperado apresentar alternativas que ajudam a manter o fluxo de caixa saudável.',
+                'Além disso, ao falar do tamanho do negócio, faltou reforçar que as soluções se adaptam a diferentes perfis, inclusive pequenos empreendimentos.',
+                'Na preocupação com a rotina, o caminho seria destacar a simplicidade da operação e o suporte disponível no dia a dia.',
+                'No geral, faltou transformar cada dúvida em uma oportunidade de apresentar os produtos e serviços da Cielo de forma estratégica.'
+            ]; 
+    }
 }
