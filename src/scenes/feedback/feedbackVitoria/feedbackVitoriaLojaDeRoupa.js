5bcb4adf8a94693b9a4824f0b2e465be04c79db7
unknown <fabianne.leite@sou.inteli.edu.br>
2026-03-26 09:08:52 -0300

    Implementa diálogo de feedback de vitória da padaria e da loja de roupa

--- src/scenes/feedback/feedbackVitoria/feedbackVitoriaLojaDeRoupa.js
+++ src/scenes/feedback/feedbackVitoria/feedbackVitoriaLojaDeRoupa.js
@@ -7,5 +7,13 @@ class FeedbackVitoriaLojaDeRoupa extends FeedbackVitoria {
     }
 
     definirFundo()    { return 'assets/lojaderoupa_interior.png'; }
-    definirDialogos() { return ['Texto de feedback de vitoria da loja de roupa...']; }
+    definirDialogos() { 
+        return [
+                    'Parabéns! Você conduziu bem a conversa e conseguiu gerar confiança com a cliente.',
+                    'Agora, você avançará para um novo desafio: a loja de roupas da Nayara.',
+                    'Nayara é dona de um vestuário com faturamento mensal em torno de 15 mil reais e carrega uma forte descendência indígena, refletida em sua história e identidade.',
+                    'Ela já teve experiência com diferentes provedores de maquininhas, o que a torna mais criteriosa na comparação de benefícios e taxas.',
+                    'Seu desafio será apresentar as soluções da Cielo de forma clara e vantajosa, mostrando diferenciais que realmente façam sentido para o negócio dela.'
+        ]; 
+    }
 }
--- src/scenes/feedback/feedbackVitoria/feedbackVitoriaPadaria.js
+++ src/scenes/feedback/feedbackVitoria/feedbackVitoriaPadaria.js
@@ -7,5 +7,13 @@ class FeedbackVitoriaPadaria extends FeedbackVitoria {
     }
 
     definirFundo()    { return 'assets/padaria_interior.png'; }
-    definirDialogos() { return ['Texto de feedback de vitoria da padaria...']; }
+    definirDialogos() { 
+        return [  
+                'Parabéns! Você conduziu bem a conversa e conseguiu gerar confiança com a cliente.',
+                'Agora, você avançará para um novo desafio: o salão da Dona Leila.',
+                'Leila é dona de um salão de beleza com faturamento mensal em torno de 20 mil reais e também lidera um grupo de apoio à comunidade LGBTQIAPN+.',
+                'Ela valoriza muito acolhimento e respeito, mas já teve experiências negativas com outros vendedores, o que a torna mais impaciente nesse tipo de abordagem.',
+                'Seu desafio será apresentar as soluções da Cielo com empatia, objetividade e foco nas necessidades do negócio, evitando erros que possam encerrar a conversa rapidamente.'
+        ]; 
+    }
 }
