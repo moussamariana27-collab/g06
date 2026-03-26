548f10d50d5affb33865ae7067ab941a78ee31aa
unknown <fabianne.leite@sou.inteli.edu.br>
2026-03-26 09:45:20 -0300

    #376 Implementa diálogo de feedback de vitória do posto de gasolina

--- src/scenes/feedback/feedbackVitoria/feedbackVitoriaPosto.js
+++ src/scenes/feedback/feedbackVitoria/feedbackVitoriaPosto.js
@@ -7,5 +7,13 @@ class FeedbackVitoriaPosto extends FeedbackVitoria {
     }
 
     definirFundo()    { return 'assets/FundoPosto.png'; }
-    definirDialogos() { return ['Texto de feedback de vitoria do posto de gasolina...']; }
+    definirDialogos() 
+    { return [
+            'Parabéns! Você concluiu todos os desafios com excelência.',
+            'Você demonstrou habilidade em identificar as necessidades de cada cliente e conectar com as soluções da Cielo.',
+            'Mesmo diante de cenários mais exigentes, como o da Sra. Roberta, manteve uma abordagem estratégica, objetiva e segura.',
+            'Ao longo da jornada, você aplicou corretamente conceitos como fluxo de caixa, antecipação, eficiência operacional e gestão do negócio.',
+            'Você está pronto para atuar com diferentes perfis de clientes e gerar valor em cada negociação.'
+        ]; 
+    }
 }
