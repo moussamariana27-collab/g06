3622cee2b6bf324bcb993325eea031083b91d95a
unknown <fabianne.leite@sou.inteli.edu.br>
2026-03-26 09:41:43 -0300

    #374 Implementa diálogo de feedback de vitória do supermercado

--- src/scenes/feedback/feedbackVitoria/feedbackVitoriaSupermercado.js
+++ src/scenes/feedback/feedbackVitoria/feedbackVitoriaSupermercado.js
@@ -7,5 +7,14 @@ class FeedbackVitoriaSupermercado extends FeedbackVitoria {
     }
 
     definirFundo()    { return 'assets/mercado_interior.png'; }
-    definirDialogos() { return ['Texto de feedback de vitoria do supermercado...']; }
+    definirDialogos() 
+        { return [
+                'Parabéns! Você conduziu bem a conversa e conseguiu gerar confiança com o cliente.',
+                'Agora, você avançará para um novo desafio: o posto da Sra. Roberta.',
+                'Roberta assumiu a gestão muito jovem após um imprevisto familiar e, com muita responsabilidade, manteve o negócio funcionando.',
+                'Hoje, o posto tem faturamento mensal em torno de 750 mil reais e uma operação de alta exigência.',
+                'Ela já recebeu diversas propostas, é bastante impaciente e difícil de convencer.',
+                'Seu desafio será apresentar as soluções da Cielo com objetividade e segurança, demonstrando valor rapidamente.'
+            ]; 
+        }
 }
